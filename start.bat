dd346db020a8bcc448334fa115e61021de950e17
Navruc Dimitri Alexis <navruc.dimitri@gmail.com>
2023-09-14 00:10:57 +0300

    Fixed start

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
-powershell -command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.11.5/python-3.11.5.exe', 'C:/Tools/python-3.11.5.exe'); & c:\Tools\python-3.11.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python362; [Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\Tools\Python362', 'Machine')"
 call pip install pynput
 call pip install requests
 python ./main.py
